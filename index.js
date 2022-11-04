ca0ac72eb47538f0cc7f5b80ccad6899726c86bd
dom alexander <dom.alexan@gmail.com>
2022-11-04 11:39:15 -0600

    commit tree efeace70ba80b0d866c5da87836c3d8a950f2d2f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+findMatching = (drivers, string) => {
+    return drivers.filter(function (driver) {return driver.toLowerCase() === string.toLowerCase()});
+  };
+
+function fuzzyMatch (drivers, string) {
+    return drivers.filter(function (driver) {return driver.toLowerCase().substring(0, string.length) === string.toLowerCase()});
+  }
+
+/*fuzzyMatch = (drivers, string) => {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, string.length) === string.toLowerCase() })
+}*/
+
+function matchName(drivers, para) {
+    return drivers.filter(function (driver) { return driver.name === para });
+}
